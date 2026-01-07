aed4be890863e20eaad540d1cc55c8974ec05a96
tusharn3115 <negitushar81@gmail.com>
2026-01-07 20:56:59 +0530

    feat: Add smooth scrolling functionality using the Lenis library.

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AllProjects from './components/projects/AllProjects';
 
 import { AnimatePresence, motion } from 'motion/react';
 import CustomCursor from './components/ui/CustomCursor';
+import SmoothScroll from './components/ui/SmoothScroll';
 
 // Scroll to top on route change
 const ScrollToTop = () => {
@@ -32,6 +33,7 @@ const AppContent = () => {
   return (
     <>
       <ScrollToTop />
+      <SmoothScroll />
       {/* <CustomCursor /> */}
       <div className="noise-bg-fixed" />
 
